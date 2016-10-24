b6823570dc7403efc7b93ab332c8b4c8734962aa
haozb <haozhongbao@innobuddy.com>
2016-10-24 16:26:42 +0800

    weixin index

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ app.get('/', function (req, res) {
   res.send('hello');
 });
 
-app.listen(3000);
+//signature 	微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+//timestamp 	时间戳
+//nonce 	随机数
+//echostr 	随机字符串
+app.get('/weixin', function (req, res) {
+  console.log('req.query', req.query);
+  res.send(req.echostr);
+});
 
-console.log('start: 3000');
+app.listen(80);
+
+console.log('---start---');
